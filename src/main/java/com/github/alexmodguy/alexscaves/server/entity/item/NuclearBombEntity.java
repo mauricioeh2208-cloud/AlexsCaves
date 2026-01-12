40c0d5c1c316a550e22ed20169ab4e93557c62ac
Alex <islicy@outlook.com>
2026-01-12 16:38:07 +0900

    Fix attachment point calculation in NuclearBombEntity to improve passenger movement accuracy

--- src/main/java/com/github/alexmodguy/alexscaves/server/entity/item/NuclearBombEntity.java
+++ src/main/java/com/github/alexmodguy/alexscaves/server/entity/item/NuclearBombEntity.java
@@ -177,7 +177,8 @@ public class NuclearBombEntity extends Entity {
             float expandScale = 1F + (float) Math.sin(progress * progress * Math.PI) * 0.5F;
             float f1 = -(this.getXRot() / 40F);
             float j = expandScale - progress * 0.3F;
-            double d0 = this.getY() + j + 0.5F - 0.2F;
+            Vec3 attachPoint = passenger.getVehicleAttachmentPoint(this);
+            double d0 = this.getY() + j - attachPoint.y;
             moveFunction.accept(passenger, this.getX(), d0, this.getZ());
             passenger.fallDistance = 0.0F;
         } else {

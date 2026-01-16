f906f58ec5aa75b10e9d60d2582485c51ab5a0b3
Alex <islicy@outlook.com>
2026-01-16 19:03:39 +0900

    Update rainbow render type to use TRANSLUCENT_TRANSPARENCY for improved appearance

--- src/main/java/com/github/alexmodguy/alexscaves/client/render/ACRenderTypes.java
+++ src/main/java/com/github/alexmodguy/alexscaves/client/render/ACRenderTypes.java
@@ -206,7 +206,8 @@ public class ACRenderTypes extends RenderType {
     }
 
     public static RenderType getRainbow(ResourceLocation resourceLocation) {
-        return create("rainbow", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, RenderType.CompositeState.builder().setShaderState(RenderStateShard.RENDERTYPE_ENERGY_SWIRL_SHADER).setTextureState(new RenderStateShard.TextureStateShard(resourceLocation, false, true)).setLightmapState(LIGHTMAP).setCullState(RenderStateShard.NO_CULL).setTransparencyState(EYES_ALPHA_TRANSPARENCY).setDepthTestState(LEQUAL_DEPTH_TEST).createCompositeState(true));
+        // Use TRANSLUCENT_TRANSPARENCY like 1.20 for more solid rainbow appearance
+        return create("rainbow", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, RenderType.CompositeState.builder().setShaderState(RenderStateShard.RENDERTYPE_ENERGY_SWIRL_SHADER).setTextureState(new RenderStateShard.TextureStateShard(resourceLocation, false, true)).setLightmapState(LIGHTMAP).setCullState(RenderStateShard.NO_CULL).setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY).setDepthTestState(LEQUAL_DEPTH_TEST).createCompositeState(true));
     }
 
     public static RenderType getHologram(ResourceLocation locationIn) {
